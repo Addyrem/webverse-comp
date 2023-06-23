d94e2cdeec4a1fcaa9ca5ccba1ea0bffb5da1b30
aditya <adityakhanna077@gmail.com>
2023-06-24 05:20:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('section1').addEventListener('click', function() {
+    alert('You clicked on Students!');
+  });
+  
+  document.getElementById('section2').addEventListener('click', function() {
+    alert('You clicked on Faculty!');
+  });
+  
+  document.getElementById('section3').addEventListener('click', function() {
+    alert('You clicked on Warden!');
+  });
+  
